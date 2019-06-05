2fdbbd08d9e40553ef8a7f0fab8e078ec581a0f2
Rachel Car <rachel.c.carr@hotmail.com>
2019-06-05 17:36:21 +0100

    Create Form1.cs

---
+++ Assignment 2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Assignment_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            var Form = new Form2();
+            Form.Show(this);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var Form = new Form3();
+            Form.Show(this);
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

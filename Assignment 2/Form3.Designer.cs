65de12bc776c0ace7e50e35499c22b5fa9120113
Rachel Car <rachel.c.carr@hotmail.com>
2019-06-05 17:36:33 +0100

    Create Form3.Designer.cs

---
+++ Assignment 2/Form3.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Assignment_2
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.explaination = new System.Windows.Forms.TextBox();
+            this.buttonNormal = new System.Windows.Forms.Button();
+            this.buttonHard = new System.Windows.Forms.Button();
+            this.Easy = new System.Windows.Forms.Button();
+            this.Normal = new System.Windows.Forms.Button();
+            this.Hard = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Easybutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(157, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(167, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Choose your difficulty:";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // explaination
+            // 
+            this.explaination.Location = new System.Drawing.Point(137, 12);
+            this.explaination.Multiline = true;
+            this.explaination.Name = "explaination";
+            this.explaination.ReadOnly = true;
+            this.explaination.Size = new System.Drawing.Size(215, 33);
+            this.explaination.TabIndex = 1;
+            this.explaination.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.explaination.Visible = false;
+            // 
+            // buttonNormal
+            // 
+            this.buttonNormal.Location = new System.Drawing.Point(199, 103);
+            this.buttonNormal.Name = "buttonNormal";
+            this.buttonNormal.Size = new System.Drawing.Size(75, 23);
+            this.buttonNormal.TabIndex = 3;
+            this.buttonNormal.Text = "Normal";
+            this.buttonNormal.UseVisualStyleBackColor = true;
+            this.buttonNormal.Click += new System.EventHandler(this.buttonNormal_Click);
+            // 
+            // buttonHard
+            // 
+            this.buttonHard.Location = new System.Drawing.Point(199, 155);
+            this.buttonHard.Name = "buttonHard";
+            this.buttonHard.Size = new System.Drawing.Size(75, 23);
+            this.buttonHard.TabIndex = 4;
+            this.buttonHard.Text = "Hard";
+            this.buttonHard.UseVisualStyleBackColor = true;
+            this.buttonHard.Click += new System.EventHandler(this.buttonHard_Click);
+            // 
+            // Easy
+            // 
+            this.Easy.Location = new System.Drawing.Point(361, 161);
+            this.Easy.Name = "Easy";
+            this.Easy.Size = new System.Drawing.Size(75, 23);
+            this.Easy.TabIndex = 5;
+            this.Easy.Text = "Enter";
+            this.Easy.UseVisualStyleBackColor = true;
+            this.Easy.Visible = false;
+            this.Easy.Click += new System.EventHandler(this.Easy_Click);
+            // 
+            // Normal
+            // 
+            this.Normal.Location = new System.Drawing.Point(361, 161);
+            this.Normal.Name = "Normal";
+            this.Normal.Size = new System.Drawing.Size(75, 23);
+            this.Normal.TabIndex = 6;
+            this.Normal.Text = "Easy";
+            this.Normal.UseVisualStyleBackColor = true;
+            this.Normal.Visible = false;
+            this.Normal.Click += new System.EventHandler(this.Normal_Click);
+            // 
+            // Hard
+            // 
+            this.Hard.Location = new System.Drawing.Point(361, 161);
+            this.Hard.Name = "Hard";
+            this.Hard.Size = new System.Drawing.Size(75, 23);
+            this.Hard.TabIndex = 7;
+            this.Hard.Text = "Enter";
+            this.Hard.UseVisualStyleBackColor = true;
+            this.Hard.Visible = false;
+            this.Hard.Click += new System.EventHandler(this.Hard_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(188, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Output";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Input";
+            this.label2.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(187, 132);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Visible = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(188, 190);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.Visible = false;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(361, 53);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(53, 20);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.Text = "No. Lives:";
+            this.textBox5.Visible = false;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(411, 53);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(36, 20);
+            this.textBox6.TabIndex = 14;
+            this.textBox6.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Easybutton
+            // 
+            this.Easybutton.Location = new System.Drawing.Point(199, 50);
+            this.Easybutton.Name = "Easybutton";
+            this.Easybutton.Size = new System.Drawing.Size(75, 23);
+            this.Easybutton.TabIndex = 17;
+            this.Easybutton.Text = "Easy";
+            this.Easybutton.UseVisualStyleBackColor = true;
+            this.Easybutton.Click += new System.EventHandler(this.Easybutton_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 260);
+            this.Controls.Add(this.Easybutton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Hard);
+            this.Controls.Add(this.Normal);
+            this.Controls.Add(this.Easy);
+            this.Controls.Add(this.buttonHard);
+            this.Controls.Add(this.buttonNormal);
+            this.Controls.Add(this.explaination);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form3";
+            this.Text = "Conundrum";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox explaination;
+        private System.Windows.Forms.Button buttonNormal;
+        private System.Windows.Forms.Button buttonHard;
+        private System.Windows.Forms.Button Easy;
+        private System.Windows.Forms.Button Normal;
+        private System.Windows.Forms.Button Hard;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Easybutton;
+    }
+}
